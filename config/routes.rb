8483968e6db1d6bee91e33577929030a61fe2b97
EC2 Default User <ec2-user@ip-172-31-18-113.ap-northeast-1.compute.internal>
2023-01-07 23:59:21 +0000

    Update layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+   # 顧客用
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  # 管理者用
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: 'admin/sessions'
+  }
+
   namespace :admin do
     resources :orders, only: [:show, :update]
   end
@@ -36,16 +47,6 @@ Rails.application.routes.draw do
     root to: 'homes#top'
     get '/about' => 'homes#about', as: 'about'
   end
-  # 顧客用
-  devise_for :customers, skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: 'public/sessions'
-  }
-
-  # 管理者用
-  devise_for :admin, skip: [:registrations, :passwords], controllers: {
-    sessions: 'admin/sessions'
-  }
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
