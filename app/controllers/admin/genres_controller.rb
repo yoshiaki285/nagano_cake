f15961c3ed6a37cdbc928957b096a7c73441a0ac
EC2 Default User <ec2-user@ip-172-31-18-113.ap-northeast-1.compute.internal>
2023-01-13 11:42:46 +0000

    Update admin/genres index

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,7 +1,17 @@
 class Admin::GenresController < ApplicationController
   def index
+    @genre = Genre.new
   end
 
   def edit
+
+  end
+
+  def create
+
+  end
+
+  def update
+
   end
 end
--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -13,9 +13,12 @@ class Public::CustomersController < ApplicationController
 
   def withdraw
   end
-end
 
 
-def customer_params
-    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number)
+  private
+  
+  def customer_params
+      params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number)
+  end
+
 end
