93f54e74189dd38c2dc170cf94107679e108c654
EC2 Default User <ec2-user@ip-172-31-18-113.ap-northeast-1.compute.internal>
2023-01-14 09:47:08 +0000

    Update genre index edit

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,17 +1,30 @@
 class Admin::GenresController < ApplicationController
   def index
     @genre = Genre.new
+    @genres = Genre.all
   end
 
   def edit
-
+    @genre = Genre.find(params[:id])
   end
 
   def create
-
+    @genre = Genre.new(genre_params)
+    @genres = Genre.all
+    @genre.save
+    render :index
   end
 
   def update
-
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params)
+    redirect_to admin_genres_path
   end
+
+  private
+
+    def genre_params
+      params.require(:genre).permit(:name)
+    end
+
 end
