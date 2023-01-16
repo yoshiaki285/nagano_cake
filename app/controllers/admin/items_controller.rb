f6a7ea33e935f14c6ddaf277ccc7f89980d1c34b
EC2 Default User <ec2-user@ip-172-31-18-113.ap-northeast-1.compute.internal>
2023-01-16 12:08:54 +0000

    Update item/new

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -3,11 +3,25 @@ class Admin::ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+    @genres = Genre.all
   end
 
   def show
   end
 
   def edit
+  end
+  
+  def create
+  end
+  
+  def update
+  end
+  
+  private
+
+  def item_params
+      params.require(:item).permit(:name, :introduction, :price)
   end
 end
