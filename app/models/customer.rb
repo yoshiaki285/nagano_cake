b1dc45d6da6651e15bc11f34092670c41492aea0
EC2 Default User <ec2-user@ip-172-31-36-154.ap-northeast-1.compute.internal>
2023-01-22 00:54:53 +0000

    Update admin/customer/index,show,edit

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,5 +8,12 @@ class Customer < ApplicationRecord
   has_many :addresses, dependent: :destroy
   has_many :orders, dependent: :destroy
   
+  def full_name
+    self.last_name + " " + self.first_name
+  end
+  
+  def full_name_kana
+    self.last_name_kana + " " + self.first_name_kana
+  end
   
 end
