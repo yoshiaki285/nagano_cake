d42cb1d1c03855b6a243149d3a89e2853f496038
EC2 Default User <ec2-user@ip-172-31-36-154.ap-northeast-1.compute.internal>
2023-01-24 03:52:58 +0000

    Update address/index

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,35 @@
 class Public::AddressesController < ApplicationController
   def index
+    @addresses = Address.all
+    @customer = current_costomer
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
+  
+  def create
+    @address = Address.new(address_params)
+    @address.save
+    redirect_to addresses_path
+  end
+  
+  def update
+    @address = Address.find(params[:id])
+    @address.update(address_params)
+    redirect_to addresses_path
+  end
+  
+  def destroy
+    @address = Address.find(params[:id])
+    @address.delete
+    redirect_to addresses_path
+  end
+  
+  private
+  
+  def address_params
+    params.require(:address).permit(:costomer_id, :name, :postal_code, :address)
+  end
+  
 end
