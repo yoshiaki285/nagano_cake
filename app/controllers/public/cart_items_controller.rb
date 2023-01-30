9676e756ca49a68935bde46a1e83418434e0374a
EC2 Default User <ec2-user@ip-172-31-36-154.ap-northeast-1.compute.internal>
2023-01-30 11:14:14 +0000

    Update customer cart_item/index

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -7,32 +7,33 @@ class Public::CartItemsController < ApplicationController
   def create
     # binding.pry
     @item = Item.find(cart_item_params[:item_id])
-    @cart_item = CartItem.new(cart_item_params)
-    @cart_item.customer_id = current_customer.id
-    @cart_items = current_customer.cart_items
-      @cart_items.each do |cart_item|
-        if Item.find_by(name: @cart_item.item.name)
-          cart_item.amount += @cart_item.amount.to_i
-          cart_item.save
-        else
-          @cart_item.save
-        end
-      end
-      redirect_to cart_items_path
+    if CartItem.exists?(customer_id: current_customer.id, item_id: @item.id)
+      @cart_item = CartItem.find_by(customer_id: current_customer.id, item_id: @item.id)
+      @cart_item.amount += params[:cart_item][:amount].to_i
+      @cart_item.save
+    else
+      @cart_item = CartItem.new(cart_item_params)
+      @cart_item.customer_id = current_customer.id
+      @cart_item.save
+    end
+    redirect_to cart_items_path
   end
   
   def update
-    
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to cart_items_path
   end
   
   def destroy
-    
-    
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.delete
+    redirect_to cart_items_path
   end
   
   def destroy_all
     @cart_items = current_customer.cart_items
-    current_customer.cart_items.destroy_all
+    @cart_items.destroy_all
     redirect_to items_path
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -37,8 +37,8 @@ Rails.application.routes.draw do
     resources :orders, only: [:new, :create, :index, :show]
   end
   scope module: :public do
-    resources :cart_items, only: [:index, :update, :destroy, :create]
     delete '/cart_items/destroy_all' => 'cart_items#destroy_all'
+    resources :cart_items, only: [:index, :update, :destroy, :create]
   end
   scope module: :public do
     get '/customers/my_page' => 'customers#show'
