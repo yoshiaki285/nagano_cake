e41755cc1759723d0abbdaddf30722b05228135d
EC2 Default User <ec2-user@ip-172-31-36-154.ap-northeast-1.compute.internal>
2023-02-12 01:33:12 +0000

    Update admin/sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,14 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number])
   end
+  
+  private
+  
+  def after_sign_in_path_for(resource_or_scope)
+    if resource_or_scope.is_a?(Admin)
+      admin_path
+    else
+      root_path
+    end
+  end
 end
