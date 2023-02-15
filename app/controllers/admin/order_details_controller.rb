1cb98c196b36fd2df0924ebdc8d51c42a1af8f7f
EC2 Default User <ec2-user@ip-172-31-36-154.ap-northeast-1.compute.internal>
2023-02-15 11:25:21 +0000

    Update admin/order order_detail controller

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -2,7 +2,19 @@ class Admin::OrderDetailsController < ApplicationController
   
   def update
     @order_detail = OrderDetail.find(params[:id])
-    @order_detail.update(order_detail_params)
+    @order = @order_detail.order
+    @order_details = @order.order_details.all
+    
+    is_updated = true
+    if  @order_detail.update(order_detail_params)
+      @order.update(order_status: 2) if @order_detail.product_status == "in_product"
+      @order_details.each do |order_detail|
+        if order_detail.product_status != "complete"
+          is_updated = false
+        end
+      end
+      @order.update(order_status: 3) if is_updated
+    end
     redirect_to admin_order_path(@order_detail.order.id)
   end
   
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,21 +1,22 @@
 class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
-    @order_detail = OrderDetail.find(params[:id])
+    @order_detail = OrderDetail.where(params[:id])
   end
   
   def update
     @order = Order.find(params[:id])
-    @order.update(order_status_params)
+    @order_details = OrderDetail.where(order_id: params[:id])
+    if @order.update(order_params)
+      @order_details.update_all(product_status: 1) if @order.order_status == "pay_confirm"
+    end
     redirect_to admin_order_path(@order.id)
   end
   
   private
   
-  def order_status_params
+  def order_params
     params.require(:order).permit(:order_status)
   end
-  
-  
   
 end
